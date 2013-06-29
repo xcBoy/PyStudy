be850981892acdaa6641582713cfa249853a1406
xcBoy <czeng99@live.com>
2013-06-29 22:38:08 +0800

    Commit

--- xcBoy/LangStudy/Python/FirstSample.py
+++ xcBoy/LangStudy/Python/FirstSample.py
@@ -3,7 +3,11 @@ Created on 2013年6月29日
 
 @author: zengchen
 '''
-
+import sys
+import random
 if __name__ == '__main__':
     print("Hello , World !")
+    print(2 ** 100)
+    print(sys.platform)
+    print(random.choice([1,2,3,4]))
     pass
